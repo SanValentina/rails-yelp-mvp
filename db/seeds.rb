fb3dc5bf755ab954629a9500ad9f1c6111c1cf23
Valentina Antollini <antolliniv@hotmail.com>
2018-08-23 19:06:51 +0100

    sfghghfshfg

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,42 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  32904812093,
+    number:        1,
+    category:      'chinese'
+  },
+
+  {
+    name:         'Rosso Pomodoro',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  32904812093,
+    number:        1,
+    category:      'chinese'
+  },
+
+  {
+    name:         'On the Bab',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  32904812093,
+    number:        1,
+    category:      'chinese'
+  },
+
+  {
+    name:         'Bun Bun',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  32904812093,
+    number:        1,
+    category:      'chinese'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
